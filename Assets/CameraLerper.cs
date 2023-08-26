320cb03c910d77f3159773803c4c8bf72b11c2cc
JesusJMUJI <al427616@uji.es>
2023-08-26 18:04:00 +0200

    Set scene fps to 144 and fix coral seams

--- Assets/CameraLerper.cs
+++ Assets/CameraLerper.cs
@@ -10,7 +10,7 @@ public class CameraLerper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 144;
     }
 
     // Update is called once per frame
