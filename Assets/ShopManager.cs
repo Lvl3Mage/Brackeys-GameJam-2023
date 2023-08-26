b6eac122683dcc80d1c1bfeffe329c62e0ba3ff2
Lvl3Mage <lvlthreemage@gmail.com>
2023-08-26 04:13:34 +0200

    Modified store manager to work with notifications

--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -6,20 +6,37 @@ using UnityEngine.UIElements;
 
 public class ShopManager : MonoBehaviour
 {
-    public GameObject shopManagerPanel;
-    void Start()
-    {
-        shopManagerPanel.SetActive(false);
-    }
-    
-    public void OpenShop()
-    {
-        // On Something do open the shop
-        shopManagerPanel.SetActive(true);
-    }
-    public void CloseShop()
-    {
-        // On Something do close the shop
-        shopManagerPanel.SetActive(false);
-    }
+	[SerializeField] CanvasGroup shopManagerPanel;
+	[SerializeField] CanvasGroup shopNotification;
+	[SerializeField] float accessibleDepth;
+	[SerializeField] float notificationFadeSpeed;
+	void Start()
+	{
+		ToggleShop(false);
+	}
+	bool shopOpen = false;
+	void Update(){
+		bool shopAccessible = PlayerInfo.GetPlayerPosition().y > accessibleDepth;
+		shopNotification.alpha = Mathf.Lerp(shopNotification.alpha,shopAccessible ? 1 : 0, notificationFadeSpeed*Time.deltaTime);
+		if(Input.GetKeyDown(KeyCode.Space) && shopAccessible){
+			ToggleShop(!shopOpen);
+		}
+	}
+	
+	public void Open()
+	{
+		// On Something do open the shop
+		ToggleShop(true);
+	}
+	public void Close()
+	{
+		// On Something do close the shop
+		ToggleShop(false);
+	}
+	void ToggleShop(bool value){
+		shopOpen = value;
+		shopManagerPanel.alpha = value ? 1 : 0;
+		shopManagerPanel.interactable = value;
+		shopManagerPanel.blocksRaycasts = value;
+	}
 }
