f38c1f8034886f996eff6382ea73379d706a9d1e
Lvl3Mage <lvlthreemage@gmail.com>
2023-08-26 20:51:38 +0200

    Added Lag

--- Assets/BoatController.cs
+++ Assets/BoatController.cs
@@ -19,8 +19,8 @@ public class BoatController : MonoBehaviour
 		Vector2 playerPos = PlayerInfo.GetPlayerPosition();
 		transform.position = Vector3.Lerp(transform.position, new Vector3(playerPos.x, transform.position.y, transform.position.z), moveSpeed*Time.fixedDeltaTime);
 
-		float delta = playerPos.x - transform.position.x;
-		spriteRenderer.flipX = delta > 0;
+		bool inverse = (playerPos.x - transform.position.x) < 0;
+		transform.localScale = new Vector3(inverse ? 1 : -1,transform.localScale.y,transform.localScale.z);
 
 		transform.position = new Vector3(transform.position.x, bopNoise.SampleAt(transform.position), transform.position.z);
 		transform.rotation = Quaternion.Euler(0,0,rotationNoise.SampleAt(transform.position));
